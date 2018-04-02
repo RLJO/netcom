634a6af2e53354fb12b81c889d79bd7e20e11313
steve <solise@mceesolutions.com>
2018-04-02 17:02:35 +0100

    nok4

--- netcom/models/models.py
+++ netcom/models/models.py
@@ -205,7 +205,60 @@ class SubAccount(models.Model):
     def button_cancel(self):
         self.write({'state': 'cancel'})
         return {}
-    
+
+class PartnerCategory(models.Model):
+    _description = 'Partner Tags'
+    _name = 'res.partner.category'
+    _order = 'parent_left, name'
+    _parent_store = True
+    _parent_order = 'name'
+
+    name = fields.Char(string='Tag Name', required=True, translate=True)
+    color = fields.Integer(string='Color Index')
+    parent_id = fields.Many2one('res.partner.category', string='Parent Category', index=True, ondelete='cascade')
+    child_ids = fields.One2many('res.partner.category', 'parent_id', string='Child Tags')
+    active = fields.Boolean(default=True, help="The active field allows you to hide the category without removing it.")
+    parent_left = fields.Integer(string='Left parent', index=True)
+    parent_right = fields.Integer(string='Right parent', index=True)
+    partner_ids = fields.Many2many('res.partner', column1='category_id', column2='partner_id', string='Partners')
+
+    @api.constrains('parent_id')
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise ValidationError(_('Error ! You can not create recursive tags.'))
+
+    @api.multi
+    def name_get(self):
+        """ Return the categories' display name, including their direct
+            parent by default.
+
+            If ``context['partner_category_display']`` is ``'short'``, the short
+            version of the category name (without the direct parent) is used.
+            The default is the long version.
+        """
+        if self._context.get('partner_category_display') == 'short':
+            return super(PartnerCategory, self).name_get()
+
+        res = []
+        for category in self:
+            names = []
+            current = category
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append((category.id, ' / '.join(reversed(names))))
+        return res
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            args = [('name', operator, name)] + args
+        return self.search(args, limit=limit).name_get()
+
+
 class PensionManager(models.Model):
     _name = 'pen.type'
     
