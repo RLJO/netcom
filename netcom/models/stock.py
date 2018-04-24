96d7241305ec383eb850ea11050a2b529762fbf6
Michael Njoku <mnjoku@mceesolutions.com>
2018-04-24 05:33:55 +0100

    invoice changes

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -489,7 +489,55 @@ class PurchaseOrderLine(models.Model):
                 partner_ids.append(partner.id)
             self.order_id.message_post(subject=subject,body=subject,partner_ids=partner_ids)
     
-
+class AccountInvoice(models.Model):
+    _name = "account.invoice"
+    _inherit = ['account.invoice']
+    _description = "Invoice"
+    
+    @api.one
+    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding',
+                 'currency_id', 'company_id', 'date_invoice', 'type')
+    def _compute_amount(self):
+        amount_nrc = amount_mrc = 0.0
+        for line in self.invoice_line_ids:
+            if line.nrc_mrc == "MRC":
+                amount_mrc += line.price_subtotal
+            else:
+                amount_nrc += line.price_subtotal
+        self.amount_nrc = amount_nrc
+        self.amount_mrc = amount_mrc
+        round_curr = self.currency_id.round
+        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
+        self.amount_tax = sum(round_curr(line.amount_total) for line in self.tax_line_ids)
+        self.amount_total = self.amount_untaxed + self.amount_tax
+        amount_total_company_signed = self.amount_total
+        amount_untaxed_signed = self.amount_untaxed
+        if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
+            currency_id = self.currency_id.with_context(date=self.date_invoice)
+            amount_total_company_signed = currency_id.compute(self.amount_total, self.company_id.currency_id)
+            amount_untaxed_signed = currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
+        sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.amount_total_company_signed = amount_total_company_signed * sign
+        self.amount_total_signed = self.amount_total * sign
+        self.amount_untaxed_signed = amount_untaxed_signed * sign
+    
+    amount_nrc = fields.Monetary(string='Total NRC', store=True, readonly=True, compute='_compute_amount', track_visibility='onchange')
+    amount_mrc = fields.Monetary(string='Total MRC', store=True, readonly=True, compute='_compute_amount', track_visibility='onchange')
+    
+class AccountInvoiceLine(models.Model):
+    _name = "account.invoice.line"
+    _inherit = ['account.invoice.line']
+    
+    nrc_mrc = fields.Char('MRC/NRC', compute='_compute_mrc_nrc', readonly=True, store=True)
+    sub_account_id = fields.Many2one('sub.account', string='Child Account', index=True, ondelete='cascade')
+    
+    @api.one
+    @api.depends('product_id')
+    def _compute_mrc_nrc(self):
+        if self.product_id.recurring_invoice == True:
+            self.nrc_mrc = "MRC"
+        else:
+            self.nrc_mrc = "NRC"
 
 class SaleOrder(models.Model):
     _name = "sale.order"
