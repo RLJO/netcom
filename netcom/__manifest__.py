6990baca1b2b6c97e077839cda0a07d2db1de36d
Michael Njoku <mnjoku@mceesolutions.com>
2018-05-04 09:01:02 +0100

    add Billing confirmation

--- netcom/__manifest__.py
+++ netcom/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Netcom',
-    'version': '0.11999983',
+    'version': '0.11999984',
 
 
     # any module necessary for this one to work correctly
