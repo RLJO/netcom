d25416941ef5fa1ebcda455b3fcdcdcd63d62026
dontk1 <komolafetosin@gmail.com>
2018-05-26 21:38:57 +0100

    stock limitation

--- stock_limitation/models/purchase.py
+++ stock_limitation/models/purchase.py
@@ -10,5 +10,10 @@ class PurchaseOrder(models.Model):
         'done': [('readonly', True)],
         'cancel': [('readonly', True)],
     }
+
+    @api.model
+    def _default_picking_type(self):
+        return False
+
     picking_type_id = fields.Many2one('stock.picking.type', 'Deliver To', states=READONLY_STATES, required=True, \
-        help='This will determine operation type of incoming shipment')
+        default=_default_picking_type, help='This will determine operation type of incoming shipment')
