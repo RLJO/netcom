c940768549ea8a35ef8bb882e9fa7f2ea6397442
dontk1 <komolafetosin@gmail.com>
2018-05-26 21:18:33 +0100

    stock limitation

--- stock_limitation/models/purchase.py
+++ stock_limitation/models/purchase.py
@@ -3,8 +3,8 @@
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
-	_inherit = 'purchase.order'
+    _inherit = 'purchase.order'
 
-	@api.model
-	def _default_picking_type(self):
-		return
+    @api.model
+    def _default_picking_type(self):
+        return
