9b96cdf2fe3f8c8cc803fdcd07ef91757e04d4d4
dontk1 <komolafetosin@gmail.com>
2018-06-19 13:31:14 +0100

    removed org

--- netcom_hr/__manifest__.py
+++ netcom_hr/__manifest__.py
@@ -30,7 +30,7 @@
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'website': 'http://tosinkomolafe.com',
-    'depends': ['netcom_hr_contract','netcom_hr_payroll', 'hr_org_chart'],
+    'depends': ['netcom_hr_contract','netcom_hr_payroll'],
     'sequence': '1000',
     'data': [
         'views/hr_views.xml',
