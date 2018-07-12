50fad46198da3b26f2f7bebf97bd0bfb47c74059
maverick_369 <maverick_369@github.com>
2018-07-12 14:50:43 +0100

    unpost journal entries

--- netcom/__manifest__.py
+++ netcom/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Netcom',
-    'version': '0.12999970',
+    'version': '0.12999971',
 
 
     # any module necessary for this one to work correctly
