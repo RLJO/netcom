b6d86fa7ada64b1ec77579bd59418a405d0c438c
steve <solise@mceesolutions.com>
2019-03-04 12:39:01 +0100

    BDD sales report updates

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1313,7 +1313,7 @@ class SaleReport(models.Model):
     report_nrc_mrc = fields.Char('Report MRC/NRC', readonly=True)
     reports_price_subtotal = fields.Float('Report Subtotal (SALE)', readonly=True)    
     report_date = fields.Date('Report Date', readonly=True)
-    sales_target = fields.Char(string='Salesperson Target', readonly=True)
+    sales_target = fields.Float(string='Salesperson Target', readonly=True)
     upsell_sub = fields.Boolean('Upsell', readonly=True)    
     
     def _select(self):
