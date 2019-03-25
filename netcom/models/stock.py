2476d7ff6674567f8556d3f23dd5026ec1a92ef7
steve <solise@mceesolutions.com>
2019-03-25 05:22:03 +0100

    minor fix

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1081,7 +1081,7 @@ class SaleOrderLine(models.Model):
     new_sub = fields.Boolean('New?', track_visibility='onchange', copy=False)
     
     
-    
+    @api.one
     def _compute_report_subtotal(self):
         report_price_subtotal = upsell_report_price_subtotal = 0.0
         sub = self.env['sale.subscription.line'].search([('analytic_account_id.state','=','open'), ('sub_account_id.parent_id', '=', self.order_id.partner_id.id), ('sub_account_id', '=', self.sub_account_id.id), ('product_id', '=', self.product_id.id)], limit=1)
