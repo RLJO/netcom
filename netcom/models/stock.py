710a8e75387b156637ba5ddd7b552507c7035c7d
steve <solise@mceesolutions.com>
2019-03-25 05:50:25 +0100

    minor fix

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1082,9 +1082,10 @@ class SaleOrderLine(models.Model):
     
     
     @api.one
+    @api.depends('report_nrc_mrc')
     def _compute_report_subtotal(self):
         report_price_subtotal = upsell_report_price_subtotal = 0.0
-        sub = self.env['sale.subscription.line'].search([('analytic_account_id.state','=','open'), ('sub_account_id.parent_id', '=', self.order_id.partner_id.id), ('sub_account_id', '=', self.sub_account_id.id), ('product_id', '=', self.product_id.id)], limit=1)
+        sub = self.env['sale.subscription.line'].search([('analytic_account_id.state','=','open'), ('sub_account_id.parent_id', '=', self.order_id.partner_id.id), ('sub_account_id', '=', self.sub_account_id.id), ('product_id', '=', self.product_id.id)], limit=1).id
         for line in self:
             if line.report_nrc_mrc == "MRC":
                 if sub:
