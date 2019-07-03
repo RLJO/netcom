04089a42b37c024224e085caf75829ba7359dfd3
Steve <steve@MacBook-Pro-2.local>
2019-07-03 06:27:16 +0100

    update

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1231,8 +1231,8 @@ class SaleOrder(models.Model):
     account_executive_id = fields.Many2one(string='Account Executive', comodel_name='hr.employee')
     account_manager_id = fields.Char(string='Account Manager')
     upsell_sub = fields.Boolean('Upsell?', track_visibility='onchange', copy=False, store=True)
-    report_amount_mrc = fields.Monetary(string='Report Total MRC', store=False, readonly=False, track_visibility='onchange')
-    report_amount_nrc = fields.Monetary(string='Report Total NRC', store=False, readonly=False, track_visibility='onchange')
+    report_amount_mrc = fields.Monetary(string='Report Total MRC', store=True, readonly=False, track_visibility='onchange')
+    report_amount_nrc = fields.Monetary(string='Report Total NRC', store=True, readonly=False, track_visibility='onchange')
     
     
 class SaleOrderLine(models.Model):
