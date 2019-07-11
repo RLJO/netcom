b64d11d6e3b0b30eff01455511699d53e147a353
steve <solise@mceesolutions.com>
2019-07-11 10:02:13 +0100

    update

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1217,7 +1217,7 @@ class SaleOrder(models.Model):
         return super(SaleOrder, self).copy_data(default)
     
 
-    @api.multi
+    @api.onchange('report_amount_mrc')
     def _check_negatieve(self):
         if self.report_amount_mrc < 0:
             for line in self.order_line:
