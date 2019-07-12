62c0dd0182b8d38b5ec6fd23c1b5d1b9de49db74
steve <solise@mceesolutions.com>
2019-07-12 06:45:38 +0100

    update

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1269,13 +1269,13 @@ class SaleOrderLine(models.Model):
     price_review_date = fields.Date(string='Price Review Date', readonly=True, related='sub_account_id.price_review_date', store=True)
     
     @api.one
-    @api.depends('report_nrc_mrc','order_id.state')
+    @api.depends('report_nrc_mrc')
     def _compute_report_subtotal(self):
         report_price_subtotal = 0.0
         upsell_report_price_subtotal = 0.0
         sub = self.env['sale.subscription.line'].search([('analytic_account_id.state','=','open'), ('sub_account_id.parent_id', '=', self.order_id.partner_id.id), ('sub_account_id', '=', self.sub_account_id.id), ('product_id', '=', self.product_id.id)], limit=1)
         for line in self:
-            if line.order_id.state in ['sale','done']:
+            if line.order_id.state == "done":
                 line.confirmed_reports_price_subtotal = line.reports_price_subtotal 
             else:
                 if line.report_nrc_mrc == "MRC":
