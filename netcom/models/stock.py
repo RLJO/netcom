9afb32ac643b5e2e10e35c50fb0da3936035e12e
steve <solise@mceesolutions.com>
2020-03-31 15:11:38 +0100

    Bdd Sales Report updates

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1219,8 +1219,12 @@ class StockMove(models.Model):
 class SalesPersons(models.Model):
     _name = 'sales.persons'
     
-    user_id = fields.Many2one('res.users', string='Sales Person')
-    percentage = fields.Float(string='Percentage (%)')
+    def _default_user_id(self): # this method is to search the hr.employee and return the user id of the person clicking the form atm
+        self.env['res.users'].search([('id','=',self.env.uid)])
+        return self.env['res.users'].search([('id','=',self.env.uid)])
+    
+    user_id = fields.Many2one('res.users', string='Sales Person', default=_default_user_id)
+    percentage = fields.Float(string='Percentage (%)', default=100)
     order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)
     #amount = fields.Float(string='Amount', compute='_compute_amount')
           
@@ -1356,12 +1360,15 @@ class SaleOrder(models.Model):
                      'sub_account_id': line.sub_account_id.id,
                      'product_id': line.product_id.id,
                      'account_id': line.account_id.id,
-                     #'price_subtotal': self.order_line.price_subtotal * person.percentage/100,
                      'product_uom_qty': line.product_uom_qty * person.percentage/100,
-                     #'product_uom': line.product_uom,
+                     'product_uom': line.product_uom.id,
                      'price_unit': line.price_unit,
-                     #'tax_id': line.tax_id.id,
+                     'tax_id': [(6, 0, line.tax_id.ids)],
                      'discount': line.discount,
+                     'sales_person_id': person.user_id.id,
+                     'reports_price_subtotal': line.reports_price_subtotal,
+                     'new_sub': line.new_sub,
+                     'report_nrc_mrc': line.report_nrc_mrc,
                 })
     
     @api.multi
@@ -1616,7 +1623,8 @@ class ReportSaleOrderLine(models.Model):
     _description = 'Report Sales Order Line'
     _inherit = ['sale.order.line']
     
-    
+    sales_person_id = fields.Many2one('res.users', string='Sales Person')
+    reports_price_subtotal = fields.Float('Report Subtotal')
     
 class AccountReconcileModel(models.Model):
     _name = "account.reconcile.model"
@@ -1810,6 +1818,157 @@ class BDDSaleReport(models.Model):
                     s.confirmation_date,
                     s.partner_id,
                     s.user_id,
+                    s.state,
+                    l.report_nrc_mrc,
+                    l.report_date,
+                    l.price_review_date,
+                    s.company_id,
+                    s.pricelist_id,
+                    s.analytic_account_id,
+                    s.team_id,
+                    p.product_tmpl_id,
+                    users.sales_target,
+                    partner.country_id,
+                    partner.commercial_partner_id
+        """
+        return group_by_str
+
+    @api.model_cr
+    def init(self):
+        # self._table = sale_report
+        tools.drop_view_if_exists(self.env.cr, self._table)
+        self.env.cr.execute("""CREATE or REPLACE VIEW %s as (
+            %s
+            FROM ( %s )
+            %s
+            )""" % (self._table, self._select(), self._from(), self._group_by()))
+        
+
+class BDDSalesPersonReport(models.Model):
+    _name = "netcom.sales.person.report"
+    _inherit = "netcom.sale.report"
+    _description = "Netcom BDD Sales Persons Order Report"
+    _auto = False
+    _rec_name = 'date'
+    _order = 'date desc'
+
+    name = fields.Char('Order Reference', readonly=True)
+    date = fields.Datetime('Date Order', readonly=True)
+    confirmation_date = fields.Datetime('Confirmation Date', readonly=True)
+    product_id = fields.Many2one('product.product', 'Product', readonly=True)
+    sub_account_id = fields.Many2one('sub.account', 'Sub Account', readonly=True)
+    product_uom = fields.Many2one('product.uom', 'Unit of Measure', readonly=True)
+    product_uom_qty = fields.Float('Qty Ordered', readonly=True)
+    qty_delivered = fields.Float('Qty Delivered', readonly=True)
+    qty_to_invoice = fields.Float('Qty To Invoice', readonly=True)
+    qty_invoiced = fields.Float('Qty Invoiced', readonly=True)
+    partner_id = fields.Many2one('res.partner', 'Partner', readonly=True)
+    company_id = fields.Many2one('res.company', 'Company', readonly=True)
+    #user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
+    sales_person_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
+    price_total = fields.Float('Total', readonly=True)
+    price_subtotal = fields.Float('Untaxed Total', readonly=True)
+    amt_to_invoice = fields.Float('Amount To Invoice', readonly=True)
+    amt_invoiced = fields.Float('Amount Invoiced', readonly=True)
+    product_tmpl_id = fields.Many2one('product.template', 'Product Template', readonly=True)
+    categ_id = fields.Many2one('product.category', 'Product Category', readonly=True)
+    nbr = fields.Integer('# of Lines', readonly=True)
+    pricelist_id = fields.Many2one('product.pricelist', 'Pricelist', readonly=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account', readonly=True)
+    team_id = fields.Many2one('crm.team', 'Sales Channel', readonly=True, oldname='section_id')
+    country_id = fields.Many2one('res.country', 'Partner Country', readonly=True)
+    commercial_partner_id = fields.Many2one('res.partner', 'Commercial Entity', readonly=True)
+    state = fields.Selection([
+        ('draft', 'Draft Quotation'),
+        ('sent', 'Quotation Sent'),
+        ('sale', 'Sales Order'),
+        ('done', 'Sales Done'),
+        ('cancel', 'Cancelled'),
+        ], string='Status', readonly=True)
+    weight = fields.Float('Gross Weight', readonly=True)
+    volume = fields.Float('Volume', readonly=True)
+    
+    report_nrc_mrc = fields.Char('Report MRC/NRC', readonly=True)
+    reports_price_subtotal = fields.Float('Report Subtotal (SALE)', readonly=True)    
+    report_date = fields.Date('Report Date', readonly=True)
+    sales_target = fields.Float(string='Salesperson Target', readonly=True)
+    confirmed_reports_price_subtotal = fields.Float('Confirmed Report Subtotal', readonly=True)
+    price_review_date = fields.Date(string='Price Review Date', readonly=True)
+    #upsell_sub = fields.Boolean('Upsell', readonly=True)
+    
+    
+    def _select(self):
+        select_str = """
+            WITH currency_rate as (%s)
+             SELECT min(l.id) as id,
+                    l.product_id as product_id,
+                    l.sub_account_id as sub_account_id,
+                    l.price_review_date as price_review_date,
+                    l.report_nrc_mrc as report_nrc_mrc,
+                    l.report_date as report_date,
+                    t.uom_id as product_uom,
+                    sum(l.product_uom_qty / u.factor * u2.factor) as product_uom_qty,
+                    sum(l.qty_delivered / u.factor * u2.factor) as qty_delivered,
+                    sum(l.qty_invoiced / u.factor * u2.factor) as qty_invoiced,
+                    sum(l.qty_to_invoice / u.factor * u2.factor) as qty_to_invoice,
+                    sum(l.price_total / COALESCE(cr.rate, 1.0)) as price_total,
+                    sum(l.price_subtotal / COALESCE(cr.rate, 1.0)) as price_subtotal,
+                    sum(l.reports_price_subtotal / COALESCE(cr.rate, 1.0)) as reports_price_subtotal,
+                    sum(l.confirmed_reports_price_subtotal / COALESCE(cr.rate, 1.0)) as confirmed_reports_price_subtotal,
+                    sum(l.amt_to_invoice / COALESCE(cr.rate, 1.0)) as amt_to_invoice,
+                    sum(l.amt_invoiced / COALESCE(cr.rate, 1.0)) as amt_invoiced,
+                    count(*) as nbr,
+                    s.name as name,
+                    s.date_order as date,
+                    s.confirmation_date as confirmation_date,
+                    s.state as state,
+                    s.partner_id as partner_id,
+                    l.sales_person_id as sales_person_id,
+                    s.company_id as company_id,
+                    extract(epoch from avg(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
+                    t.categ_id as categ_id,
+                    s.pricelist_id as pricelist_id,
+                    s.analytic_account_id as analytic_account_id,
+                    s.team_id as team_id,
+                    p.product_tmpl_id,
+                    users.sales_target as sales_target,
+                    partner.country_id as country_id,
+                    partner.commercial_partner_id as commercial_partner_id,
+                    sum(p.weight * l.product_uom_qty / u.factor * u2.factor) as weight,
+                    sum(p.volume * l.product_uom_qty / u.factor * u2.factor) as volume
+        """ % self.env['res.currency']._select_companies_rates()
+        return select_str
+
+    def _from(self):
+        from_str = """
+                report_sale_order_line l
+                      join sale_order s on (l.order_id=s.id)
+                      join res_users users on l.sales_person_id = users.id
+                      join res_partner partner on s.partner_id = partner.id
+                        left join product_product p on (l.product_id=p.id)
+                            left join product_template t on (p.product_tmpl_id=t.id)
+                    left join product_uom u on (u.id=l.product_uom)
+                    left join product_uom u2 on (u2.id=t.uom_id)
+                    left join product_pricelist pp on (s.pricelist_id = pp.id)
+                    left join currency_rate cr on (cr.currency_id = pp.currency_id and
+                        cr.company_id = s.company_id and
+                        cr.date_start <= coalesce(s.date_order, now()) and
+                        (cr.date_end is null or cr.date_end > coalesce(s.date_order, now())))
+        """
+        return from_str
+
+    def _group_by(self):
+        group_by_str = """
+            GROUP BY l.product_id,
+                    l.order_id,
+                    l.sub_account_id,
+                    t.uom_id,
+                    t.categ_id,
+                    s.name,
+                    s.date_order,
+                    s.confirmation_date,
+                    s.partner_id,
+                    l.sales_person_id,
                     s.state,
                     l.report_nrc_mrc,
                     l.report_date,
