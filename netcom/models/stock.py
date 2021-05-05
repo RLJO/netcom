61c5c1df7d373f98f021a290e91deaa327fb7140
Steve Olise <steve@Steves-MacBook-Pro.local>
2021-05-05 09:10:22 +0100

    set lots_visible true

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -585,7 +585,8 @@ class Picking(models.Model):
             self.picking_type_id = self.env['stock.picking.type'].search([('name','=','Internal Transfer'), ('warehouse_id.company_id','=', self.env.user.company_id.id)], limit=1).id
 
     def set_lots_visible_true(self):
-        self.move_line_ids.lots_visible = True
+        for rec in self.move_line_ids:
+            rec.lots_visible = True
 
 class StockRejectionReason(models.Model):
     _name = "stock.rejection.reason"
