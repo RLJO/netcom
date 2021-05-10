0fb19e3ba53ce4051bba57a119d82c751b52545b
Steve Olise <steve@Steves-MacBook-Pro.local>
2021-05-10 21:04:28 +0100

    sub account date wizard

---
+++ netcom/wizard/subaccount_date.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class SubAccountDate(models.TransientModel):
+    _name = 'sub.account.date'
+    _description = 'Sub-Account Date'
+
+    activation_date = fields.Date(string='Activation Date')
+    perm_up_date = fields.Date(string='Permanent Activation Date')
+    
+    activate = fields.Boolean(string='Activation Date', default=True)
+    perm_up = fields.Boolean(string='Permanent Activation Date')
+
+    subaccount_id = fields.Many2one(comodel_name='sub.account', default=lambda self: self.env.context.get('active_id', None), required=True)
+
+    def action_modify_date(self):
+        if self.activate:
+            self.subaccount_id.sudo().write({'activation_date': self.activation_date})
+        
+        if self.perm_up:
+            self.subaccount_id.sudo().write({'perm_up_date': self.perm_up_date})
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'sub.account',
+            'res_id': self.subaccount_id.id,
+            'name': self.subaccount_id.display_name,
+            'view_mode': 'form',
+            'views': [(False, "form")],
+        }
