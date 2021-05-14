31bf6cec9dfef812a767e532428d8a19f423e93e
Steve Olise <steve@Steves-MacBook-Pro.local>
2021-05-14 18:46:59 +0100

    temp fix for stock valuation

--- netcom/models/stock.py
+++ netcom/models/stock.py
@@ -1284,9 +1284,10 @@ class StockMove(models.Model):
         return True
     
     def get_unit_price(self):
-        # self.price_unit = self.purchase_line_id.price_unit
-        self.price_unit = -(self.product_id.standard_price)
-        self.value = self.price_unit * self.product_uom_qty
+        self.price_unit = self.purchase_line_id.price_unit
+        # self.price_unit = self.product_id.standard_price
+        if self.value == 0.00:
+            self.value = self.price_unit * self.product_uom_qty
 
     account_id = fields.Many2one('account.account', string='Account', index=True, ondelete='cascade')
     internal_transfer = fields.Boolean('Internal Transfer?', related='picking_id.internal_transfer', readonly=1, track_visibility='onchange')
